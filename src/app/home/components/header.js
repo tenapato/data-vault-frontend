316f8786f6058a836a03a4f3fd4b6dcb904a50c9
Patricio Tena <tenapatricio@gmail.com>
2024-02-26 13:25:49 -0600

    Fixed logout button

--- src/app/home/components/header.js
+++ src/app/home/components/header.js
@@ -5,7 +5,8 @@ import { Input } from "@/components/ui/input"
 import { DropdownMenuTrigger, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuItem, DropdownMenuContent, DropdownMenu } from "@/components/ui/dropdown-menu"
 import { TableHead, TableRow, TableHeader, TableCell, TableBody, Table } from "@/components/ui/table"
 import { HomeIcon, UploadIcon, DownloadIcon, Package2Icon, BellIcon, SearchIcon } from "./icons"; // Importing icon components from another file
-import { logout } from "../../../lib/lib";
+import {logout} from "../../../lib/lib";
+import { redirect } from "next/navigation";
 
 export default function Header() {
   return (
@@ -14,18 +15,18 @@ export default function Header() {
         <Package2Icon className="h-6 w-6" />
         <span className="sr-only">Home</span>
       </Link>
-    <div className="w-full flex-1">
+      <div className="w-full flex-1">
         <form>
-            <div className="relative">
-                <SearchIcon className="absolute left-2.5 top-2.5 h-4 w-4 text-gray-500 dark:text-gray-400" />
-                <Input
-                    className="w-full bg-white shadow-none appearance-none pl-8 md:w-2/3 lg:w-1/3 dark:bg-gray-950"
-                    placeholder="Search products..."
-                    type="search"
-                />
-            </div>
+          <div className="relative">
+            <SearchIcon className="absolute left-2.5 top-2.5 h-4 w-4 text-gray-500 dark:text-gray-400" />
+            <Input
+              className="w-full bg-white shadow-none appearance-none pl-8 md:w-2/3 lg:w-1/3 dark:bg-gray-950"
+              placeholder="Search products..."
+              type="search"
+            />
+          </div>
         </form>
-    </div>
+      </div>
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button
@@ -53,12 +54,20 @@ export default function Header() {
           <DropdownMenuItem>Settings</DropdownMenuItem>
           <DropdownMenuItem>Support</DropdownMenuItem>
           <DropdownMenuSeparator />
-          <DropdownMenuItem
-          action={async () => {
-            "use server";
-            await logout();
-            redirect("/");
-          }}>Logout</DropdownMenuItem>
+          <form
+            action={async () => {
+              "use server";
+              await logout();
+              redirect("/");
+            }}
+          >
+            <div>
+              {/* <button type="submit">Logout</button> */}
+              <DropdownMenuItem>
+                <button type="submit">Logout</button>
+              </DropdownMenuItem>
+            </div>
+          </form>
         </DropdownMenuContent>
       </DropdownMenu>
     </header>
