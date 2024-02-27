7da265f5252193652b63dbb0898c3413a7f49883
Patricio Tena <tenapatricio@gmail.com>
2024-02-26 20:00:10 -0600

    fixes

--- src/app/home/components/header.js
+++ src/app/home/components/header.js
@@ -10,12 +10,12 @@ import { redirect } from "next/navigation";
 
 export default function Header() {
   return (
-    <header className="flex h-14 items-center gap-4 border-b bg-gray-100/40 px-6 dark:bg-gray-800/40">
-      <Link className="lg:hidden" href="#">
+      <div>
+      <Link className="lg:hidden" href="/home">
         <Package2Icon className="h-6 w-6" />
         <span className="sr-only">Home</span>
       </Link>
-      <div className="w-full flex-1">
+      {/* <div className="w-full flex-1">
         <form>
           <div className="relative">
             <SearchIcon className="absolute left-2.5 top-2.5 h-4 w-4 text-gray-500 dark:text-gray-400" />
@@ -26,17 +26,17 @@ export default function Header() {
             />
           </div>
         </form>
-      </div>
-      <DropdownMenu>
+      </div> */}
+      <DropdownMenu className='flex'>
         <DropdownMenuTrigger asChild>
           <Button
             className="rounded-full border border-gray-200 w-8 h-8 dark:border-gray-800"
             size="icon"
             variant="ghost"
           >
-            <img
+            {/* <img
               alt="Avatar"
-              className="rounded-full"
+              className="rounded-full bg-black"
               height="32"
               src="/placeholder.svg"
               style={{
@@ -44,7 +44,7 @@ export default function Header() {
                 objectFit: "cover",
               }}
               width="32"
-            />
+            /> */}
             <span className="sr-only">Toggle user menu</span>
           </Button>
         </DropdownMenuTrigger>
@@ -70,6 +70,6 @@ export default function Header() {
           </form>
         </DropdownMenuContent>
       </DropdownMenu>
-    </header>
+       </div>
   );
 }
--- src/app/home/page.js
+++ src/app/home/page.js
@@ -48,7 +48,6 @@ export default async function Home(){
       {/* Main content */}
       <div className="flex flex-col w-full">
         <header className="flex h-14 items-center gap-4 border-b bg-gray-100/40 px-6 dark:bg-gray-800/40">
-          {/* Header content */}
           <Header/>
         </header>
         <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6">
@@ -76,7 +75,7 @@ export default async function Home(){
                 {files.map((file, index) => (
                   <TableRow key={index}>
                     <TableCell>{file.filename}</TableCell>
-                    <TableCell>{file.filename.split('.').pop()}</TableCell>
+                    <TableCell>{file.filename.split('.').pop() ?? ""}</TableCell>
                     {/* <TableCell>{file.key}</TableCell> */}
                     <TableCell>
                       {file.size >= 1024 * 1024
